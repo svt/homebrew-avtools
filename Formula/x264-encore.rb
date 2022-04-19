c96076bafa333e12904d2218e1352ab3e4fd9529
Finn Hermansson <finn.hermansson@svt.se>
2022-04-19 11:13:58 +0200

    Bumped versions: Ffmpeg 5.0.1 x264 r3060 x265 3.5
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- Formula/x264-encore.rb
+++ Formula/x264-encore.rb
@@ -8,30 +8,26 @@ class X264Encore < Formula
   homepage "https://www.videolan.org/developers/x264.html"
   license "GPL-2.0-only"
   head "https://code.videolan.org/videolan/x264.git"
+
   stable do
     # the latest commit on the stable branch
     url "https://code.videolan.org/videolan/x264.git",
-        revision: "cde9a93319bea766a92e306d69059c76de970190"
-    version "r3011"
+        revision: "5db6aa6cab1b146e07b60cc1736a01f21da01154"
+    version "r3060"
   end
 
   depends_on "nasm" => :build
 
   conflicts_with "x264", because: "it comes with the same binary"
 
-  # update config.* and configure: add Apple Silicon support.
-  # upstream PR https://code.videolan.org/videolan/x264/-/merge_requests/35
-  # Can be removed once it gets merged into stable branch
-  patch do
-    url "https://code.videolan.org/videolan/x264/-/commit/eb95c2965299ba5b8598e2388d71b02e23c9fba7.diff?full_index=1"
-    sha256 "7cdc60cffa8f3004837ba0c63c8422fbadaf96ccedb41e505607ead2691d49b9"
+  if MacOS.version <= :high_sierra
+    # Stack realignment requires newer Clang
+    # https://code.videolan.org/videolan/x264/-/commit/b5bc5d69c580429ff716bafcd43655e855c31b02
+    depends_on "gcc"
+    fails_with :clang
   end
 
   def install
-    # Work around Xcode 11 clang bug
-    # https://bitbucket.org/multicoreware/x265/issues/514/wrong-code-generated-on-macos-1015
-    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
-
     args = %W[
       --prefix=#{prefix}
       --disable-lsmash
@@ -47,6 +43,7 @@ class X264Encore < Formula
   end
 
   test do
+    assert_match version.to_s.delete("r"), shell_output("#{bin}/x264 --version").lines.first
     (testpath/"test.c").write <<~EOS
       #include <stdint.h>
       #include <x264.h>
@@ -60,7 +57,7 @@ class X264Encore < Formula
           return 0;
       }
     EOS
-    system ENV.cc, "-L{lib}", "test.c", "-lx264", "-o", "test"
+    system ENV.cc, "-L#{lib}", "test.c", "-lx264", "-o", "test"
     system "./test"
   end
 end
