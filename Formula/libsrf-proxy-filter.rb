eba5ad277d6a4325fb003dfaee290db1af364c97
Finn Hermansson <1035185+fhermansson@users.noreply.github.com>
2022-06-03 11:44:25 +0000

    libsrf-proxy-filter: add 1.0.1 bottle.

--- Formula/libsrf-proxy-filter.rb
+++ Formula/libsrf-proxy-filter.rb
@@ -10,6 +10,13 @@ class LibsrfProxyFilter < Formula
   license "Apache-2.0"
   head "https://github.com/SVT/ffmpeg-filter-proxy-filters.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/svt/homebrew-avtools/releases/download/libsrf-proxy-filter-1.0.1"
+    rebuild 1
+    sha256 cellar: :any,                 big_sur:      "efeecdd2562cacffd138ad1f68b03f80212266e552c567b7ccba524f30e46e1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8afd2d435b9cd30615495f5b2f15960faba5af81f871ea6b6e2d8c9424402ed4"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "cairo"
