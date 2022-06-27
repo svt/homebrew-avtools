9315ba38f184604979a0e7d5f23f9782d622c95f
Gustav Grusell <gustav.grusell@svt.se>
2022-06-27 16:08:38 +0200

    Fix libmxf again
    Signed-off-by: Gustav Grusell <gustav.grusell@svt.se>

--- Formula/libmxf.rb
+++ Formula/libmxf.rb
@@ -5,7 +5,7 @@
 #
 
 class Libmxf < Formula
-  desc "A software library written by BBC Research to read and write MXF files"
+  desc "Software library written by BBC Research to read and write MXF files"
   homepage "http://sourceforge.net/p/bmxlib/home/Home/"
   url "https://downloads.sourceforge.net/project/bmxlib/bmx-snapshot-20210707/bmx-snapshot-20210707.tar.gz"
   sha256 "425ec728213fd94ffc4f125b74c0597d19e210987d17ad8d7490e501a898ce5a"
