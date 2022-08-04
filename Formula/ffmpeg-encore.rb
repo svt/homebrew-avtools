631ee802b64c2a6a11b2bc3d0f73765dccfaef1a
Fredrik Lundkvist <fredrik.lundkvist@svt.se>
2022-08-04 12:28:17 +0200

    Try using qualified name for svt-av1 tap

--- Formula/ffmpeg-encore.rb
+++ Formula/ffmpeg-encore.rb
@@ -33,7 +33,7 @@ class FfmpegEncore < Formula
   depends_on "libvorbis"
   depends_on "libvpx"
   depends_on "openssl@3"
-  depends_on "svt-av1"
+  depends_on "grusell/svt-av1"
   depends_on "x264-encore"
   depends_on "x265-encore"
   depends_on "zimg"
