8c227c7d5553fca2959365243ae9a98f7c476ffa
Fredrik Lundkvist <fredrik.lundkvist@svt.se>
2022-08-04 10:25:12 +0200

    FFMPEG 5.1
    Signed-off-by: Fredrik Lundkvist <fredrik.lundkvist@svt.se>

--- Formula/ffmpeg-encore.rb
+++ Formula/ffmpeg-encore.rb
@@ -6,7 +6,7 @@
 class FfmpegEncore < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-5.0.1.tar.xz"
+  url "https://ffmpeg.org/releases/ffmpeg-5.1.tar.xz"
   sha256 "ef2efae259ce80a240de48ec85ecb062cecca26e4352ffb3fda562c21a93007b"
   license "GPL-3.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
