d68573f8bba495d06f44b18caeee6c397174ea49
Finn Hermansson <1035185+fhermansson@users.noreply.github.com>
2023-09-26 12:19:04 +0000

    ffmpeg-encore: update 6.0_1 bottle.

--- Formula/ffmpeg-encore.rb
+++ Formula/ffmpeg-encore.rb
@@ -23,9 +23,9 @@ class FfmpegEncore < Formula
   end
 
   bottle do
-    root_url "https://github.com/svt/homebrew-avtools/releases/download/ffmpeg-encore-6.0"
-    sha256 big_sur:      "1a03ba6e5224d6d514e4754e2f0e690961184c978c07cb0067e01bcd729bed9c"
-    sha256 x86_64_linux: "4a67ba4e9062472a85c795640b512d30fdc783cc7d0676076ebe26f2d9a62fe7"
+    root_url "https://github.com/svt/homebrew-avtools/releases/download/ffmpeg-encore-6.0_1"
+    sha256 big_sur:      "a60c636efda058774768cd68b85d5f3f8d0ec36ad627549c00b526f0a4767247"
+    sha256 x86_64_linux: "0a75e2ae7bfac5132e8435f1a60a7743f75f561f88973c092bb22abda03a31ba"
   end
   option "with-ffplay", "Enable ffplay"
 
