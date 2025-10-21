d089ef9ce2b4d4dca304eda055d5fdd32c460f01
Finn Hermansson <finn.hermansson@svt.se>
2025-10-21 16:04:08 +0200

    more lint fix
    Closes #37.
    
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>
    Signed-off-by: Finn Hermansson <1035185+fhermansson@users.noreply.github.com>

--- Formula/ffmpeg-encore.rb
+++ Formula/ffmpeg-encore.rb
@@ -7,9 +7,9 @@ class FfmpegEncore < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-8.0.tar.xz"
+  sha256 "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
   license "GPL-3.0-or-later"
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
-  sha256 "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
   option "with-ffplay", "Enable ffplay"
 
   depends_on "pkgconf" => :build
