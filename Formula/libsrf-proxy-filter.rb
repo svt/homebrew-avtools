56d66cf4e9863110f886d0c112734e9a3a8b2828
Finn Hermansson <finn.hermansson@svt.se>
2026-05-11 14:40:56 +0200

    try new proxied filters
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- Formula/libsrf-proxy-filter.rb
+++ Formula/libsrf-proxy-filter.rb
@@ -5,14 +5,13 @@
 class LibsrfProxyFilter < Formula
   desc "FFmpeg proxy filter for subtitle rendering"
   homepage "https://github.com/SVT/ffmpeg-filter-proxy-filters"
-  url "https://github.com/SVT/ffmpeg-filter-proxy-filters/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "3b9f1ba0f9e21b6c2edd523fd954f24cdd241b71fb0d0f7ba1688a12ef55db26"
+  url "https://github.com/SVT/ffmpeg-filter-proxy-filters/archive/refs/heads/caching.tar.gz"
+  sha256 "81a3667d4b1ee39c8e3af8b4fc20bd48621eac7a8c323298d5f2613776aa5213"
   license "Apache-2.0"
   head "https://github.com/SVT/ffmpeg-filter-proxy-filters.git", branch: "master"
 
   bottle do
     root_url "https://github.com/svt/homebrew-avtools/releases/download/libsrf-proxy-filter-1.0.1"
-    rebuild 1
     sha256 cellar: :any,                 big_sur:      "efeecdd2562cacffd138ad1f68b03f80212266e552c567b7ccba524f30e46e1e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "8afd2d435b9cd30615495f5b2f15960faba5af81f871ea6b6e2d8c9424402ed4"
   end
@@ -20,7 +19,6 @@ class LibsrfProxyFilter < Formula
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "cairo"
-  depends_on "protobuf-c"
 
   def install
     system "cargo", "build", "--lib", "--release", "--manifest-path", "srf_filter/Cargo.toml"
--- Formula/libsvg-proxy-filter.rb
+++ Formula/libsvg-proxy-filter.rb
@@ -5,14 +5,13 @@
 class LibsvgProxyFilter < Formula
   desc "FFmpeg proxy filter for svg rendering"
   homepage "https://github.com/SVT/ffmpeg-filter-proxy-filters"
-  url "https://github.com/SVT/ffmpeg-filter-proxy-filters/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "3b9f1ba0f9e21b6c2edd523fd954f24cdd241b71fb0d0f7ba1688a12ef55db26"
+  url "https://github.com/SVT/ffmpeg-filter-proxy-filters/archive/refs/heads/caching.tar.gz"
+  sha256 "81a3667d4b1ee39c8e3af8b4fc20bd48621eac7a8c323298d5f2613776aa5213"
   license "Apache-2.0"
   head "https://github.com/SVT/ffmpeg-filter-proxy-filters.git", branch: "master"
 
   bottle do
     root_url "https://github.com/svt/homebrew-avtools/releases/download/libsvg-proxy-filter-1.0.1"
-    rebuild 1
     sha256 cellar: :any,                 big_sur:      "994433c54c6b615cf5261a0deeed176fc37679df93a9af51c6312316f9af1beb"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "d0c9e9f125efafe9e0c7fcf45e6cc6a4dedd8218535abadc2842a449726fe492"
   end
