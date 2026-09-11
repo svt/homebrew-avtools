9359f5c0c3eddff1654402ad64c8fc0a3ed42b7a
Finn Hermansson <1035185+fhermansson@users.noreply.github.com>
2026-09-11 10:12:47 +0000

    fdk-aac: add 2.0.3 bottle.

--- Formula/fdk-aac.rb
+++ Formula/fdk-aac.rb
@@ -14,6 +14,13 @@ class FdkAac < Formula
   sha256 "829b6b89eef382409cda6857fd82af84fabb63417b08ede9ea7a553f811cb79e"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/svt/homebrew-avtools/releases/download/fdk-aac-2.0.3"
+    sha256 cellar: :any, arm64_tahoe:  "62719712e724eb486a04d20e16ef62090b713cc392c8cecc1faa04ee96196141"
+    sha256 cellar: :any, arm64_linux:  "63a87b8de9c3b9df48eb13905d0dc6a36a7ca6fab8cccb43deb40b0f0d5967a2"
+    sha256 cellar: :any, x86_64_linux: "291c00bbc1e89a8ef615ece4dbc784eebb437a09c82f3641eab0d1a4f1c01e0b"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
